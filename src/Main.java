b8159d48ba4847a2b9f5e236bf8bcae26367f41a
JSKim <mkcjsk@gmail.com>
2017-07-11 13:35:35 +0900

    hello 안녕 추가

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
 /**
  * Created by user on 2017-07-11.
  */
 public class Main {
     public static void main(String[] args) {
+        System.out.println ("hello" );
+        System.out.println ("안녕" );
 
     }
 }
